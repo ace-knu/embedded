1203c30e31ab56d937f95072ad438c978338b9e4
Jehong Jeon <jehongjeon27@gmail.com>
2023-06-13 13:41:17 +0900

    Add Example DHT11

---
+++ embedded_mcu/tc275/workspace/tc275_DHT11_Read/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/subdir.mk
@@ -0,0 +1,130 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxAsclin_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxCcu6_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxCif_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxCpu_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxDma_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxEmem_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxEray_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxFlash_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxGtm_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxHssl_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxI2c_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxMsc_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxMtu_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxMultican_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxPort_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxPsi5_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxQspi_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxScu_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxStm_cfg.c \
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxVadc_cfg.c 
+
+COMPILED_SRCS += \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxAsclin_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCcu6_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCif_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCpu_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxDma_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEmem_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEray_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxFlash_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxGtm_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxHssl_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxI2c_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMsc_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMtu_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMultican_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPort_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPsi5_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxQspi_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxScu_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxStm_cfg.src \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxVadc_cfg.src 
+
+C_DEPS += \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxAsclin_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCcu6_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCif_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCpu_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxDma_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEmem_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEray_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxFlash_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxGtm_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxHssl_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxI2c_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMsc_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMtu_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMultican_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPort_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPsi5_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxQspi_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxScu_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxStm_cfg.d \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxVadc_cfg.d 
+
+OBJS += \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxAsclin_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCcu6_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCif_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCpu_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxDma_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEmem_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEray_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxFlash_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxGtm_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxHssl_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxI2c_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMsc_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMtu_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMultican_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPort_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPsi5_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxQspi_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxScu_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxStm_cfg.o \
+./Libraries/iLLD/TC27D/Tricore/_Impl/IfxVadc_cfg.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Libraries/iLLD/TC27D/Tricore/_Impl/%.src: ../Libraries/iLLD/TC27D/Tricore/_Impl/%.c Libraries/iLLD/TC27D/Tricore/_Impl/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING C/C++ Compiler'
+	cctc -cs --dep-file="$(basename $@).d" --misrac-version=2004 -D__CPU__=tc27xd "-fD:/PersonalFiles/jehongjeon/AURIX Studio/workspace/tc275_DHT11_Read/Debug/TASKING_C_C___Compiler-Include_paths.opt" --iso=99 --c++14 --language=+volatile --exceptions --anachronisms --fp-model=3 -O0 --tradeoff=4 --compact-max-size=200 -g -Wc-w544 -Wc-w557 -Ctc27xd -Y0 -N0 -Z0 -o "$@" "$<" && \
+	if [ -f "$(basename $@).d" ]; then sed.exe -r  -e 's/\b(.+\.o)\b/Libraries\/iLLD\/TC27D\/Tricore\/_Impl\/\1/g' -e 's/\\/\//g' -e 's/\/\//\//g' -e 's/"//g' -e 's/([a-zA-Z]:\/)/\L\1/g' -e 's/\d32:/@TARGET_DELIMITER@/g; s/\\\d32/@ESCAPED_SPACE@/g; s/\d32/\\\d32/g; s/@ESCAPED_SPACE@/\\\d32/g; s/@TARGET_DELIMITER@/\d32:/g' "$(basename $@).d" > "$(basename $@).d_sed" && cp "$(basename $@).d_sed" "$(basename $@).d" && rm -f "$(basename $@).d_sed" 2>/dev/null; else echo 'No dependency file to process';fi
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Libraries/iLLD/TC27D/Tricore/_Impl/%.o: ./Libraries/iLLD/TC27D/Tricore/_Impl/%.src Libraries/iLLD/TC27D/Tricore/_Impl/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-Libraries-2f-iLLD-2f-TC27D-2f-Tricore-2f-_Impl
+
+clean-Libraries-2f-iLLD-2f-TC27D-2f-Tricore-2f-_Impl:
+	-$(RM) ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxAsclin_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxAsclin_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxAsclin_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCcu6_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCcu6_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCcu6_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCif_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCif_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCif_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCpu_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCpu_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxCpu_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxDma_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxDma_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxDma_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEmem_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEmem_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEmem_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEray_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEray_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxEray_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxFlash_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxFlash_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxFlash_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxGtm_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxGtm_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxGtm_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxHssl_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxHssl_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxHssl_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxI2c_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxI2c_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxI2c_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMsc_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMsc_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMsc_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMtu_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMtu_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMtu_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMultican_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMultican_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxMultican_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPort_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPort_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPort_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPsi5_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPsi5_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxPsi5_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxQspi_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxQspi_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxQspi_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxScu_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxScu_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxScu_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxStm_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxStm_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxStm_cfg.src ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxVadc_cfg.d ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxVadc_cfg.o ./Libraries/iLLD/TC27D/Tricore/_Impl/IfxVadc_cfg.src
+
+.PHONY: clean-Libraries-2f-iLLD-2f-TC27D-2f-Tricore-2f-_Impl
+
